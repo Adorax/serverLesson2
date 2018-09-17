7e8c6e4357495a1a24614e8f1a07f4f0e3371381
Adorax <jeremy.maret@gmail.com>
2018-09-17 16:56:52 +0300

    Add Spring project from start.sping.io

---
+++ src/main/java/tech/maret/srv_les2/SrvLes2Application.java
@@ -0,0 +1,12 @@
+package tech.maret.srv_les2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvLes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvLes2Application.class, args);
+	}
+}
